c10861b3c6b169775290d70680f9943d0ca879e8
Saravanan.B <167774818+Saravanannus@users.noreply.github.com>
2024-09-03 20:47:02 +0800

    Delete slshandler.js

--- slshandler.js
+++
@@ -1,9 +0,0 @@
-exports.sara = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v4! Your function executed successfully!",
-      }),
-    };
-  };
-  
